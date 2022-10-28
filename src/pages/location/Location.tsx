07a9abb2ad8be99e7a048bce0f61be9a9a18cc4d
Serhii Shcherbaniuk <serhii.s@elogic.com.ua>
2022-10-28 20:04:49 +0300

    - location page styles fixed

--- src/pages/location/Location.tsx
+++ src/pages/location/Location.tsx
@@ -14,7 +14,6 @@ import { useIntl } from 'react-intl';
 import { ReactComponent as Arrow } from '../../icons/ArrowWhite.svg';
 import { LOCATIONS } from '../../constants/contants';
 import { Donate } from '../common/Donate';
-import { LiveTours } from '../main/LiveTours';
 import { Message } from '../../components/message/Message';
 
 export const Location: React.FC = () => {
@@ -78,8 +77,14 @@ export const Location: React.FC = () => {
           style={{ minWidth: '100%', maxWidth: '100%' }}
         />
       </Box>
-      <Container sx={{ zIndex: 1, position: 'relative', pt: 6 }}>
-        <Stack direction="row" alignItems="center">
+      <Container sx={{ zIndex: 1, position: 'relative' }}>
+        <Box
+          sx={{
+            borderBottom: '2px solid #ffffff',
+            pb: 12,
+          }}
+        />
+        <Stack direction="row" alignItems="center" my={6}>
           <IconButton onClick={() => navigate('/')} sx={{ ml: -1 }}>
             <Arrow height={xs ? 24 : 48} width={xs ? 24 : 48} />
           </IconButton>

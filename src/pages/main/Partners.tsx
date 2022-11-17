f4bee50158b960bfe9e107d2ec4a88556c6e3791
Rustam Abduvaliiev <r.abduvaliiev@gmail.com>
2022-11-17 22:42:41 +0200

    WIP

--- src/pages/main/Partners.tsx
+++ src/pages/main/Partners.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
-import { Box, Container, Grid, Stack, Typography } from '@mui/material';
+import { Box, Container, Grid, Link, Stack, Typography } from '@mui/material';
 import { Message } from '../../components/message/Message';
 
 const PARTNERS = [
-  '/images/partners/meta_history.png',
-  '/images/partners/army_inform.png',
-  '/images/partners/media_inventor.png',
-  '/images/partners/nova_film.png',
+  {
+    image: '/images/partners/meta_history.png',
+    url: 'https://metahistory.gallery',
+  },
+  {
+    image: '/images/partners/army_inform.png',
+    url: 'https://armyinform.com.ua/',
+  },
+  {
+    image: '/images/partners/media_inventor.png',
+    url: 'https://www.facebook.com/mediainventor/',
+  },
+  {
+    image: '/images/partners/nova_film.png',
+    url: 'https://www.novafilm.com.ua/',
+  },
 ];
 export const Partners: React.FC = () => (
   <Container
@@ -37,14 +49,20 @@ export const Partners: React.FC = () => (
         }}
       >
         {PARTNERS.map((partner) => (
-          <Grid item xs={6} md={6} lg={3} key={partner}>
-            <Stack justifyContent="center" height="100%" px="25%">
-              <img
-                src={partner}
-                alt=""
-                style={{ width: '100%', height: 'auto', objectFit: 'contain' }}
-              />
-            </Stack>
+          <Grid item xs={6} md={6} lg={3} key={partner.url}>
+            <Link>
+              <Stack justifyContent="center" height="100%" px="25%">
+                <img
+                  src={partner.image}
+                  alt=""
+                  style={{
+                    width: '100%',
+                    height: 'auto',
+                    objectFit: 'contain',
+                  }}
+                />
+              </Stack>
+            </Link>
           </Grid>
         ))}
       </Grid>
